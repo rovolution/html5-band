4a327f04c312f77dc665375bfa8ebcd61c900711
Captain Deploy <ed@socialtables.com>
2014-05-24 15:20:46 -0400

    add svg keyboard

---
+++ public/js/create.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('#create-button').click(function() {
+    	var bandName = $("#band-name-input").val();
+    	console.log("bandname", bandName);
+    	$.post('/create', bandName);
+    });
+});
+
+
+// connect the socket.io server
+var socket = io.connect('http://localhost')
+//define socket events
+
+socket.on("connect",function(){
+	console.log("-----------");
+
+});
+
+// attach events
+socket.on("serverMessage",function(data){
+
+});
+socket.on("connected",function(data){
+console.log("----data",data);
+});
+socket.on("disconnected",function(data){
+
+});
+socket.on("id",function(data){
+
+});
