f715a6e75885b5b8a95dfaa94504e60e720c2c6b
Rohit Kalkur <rohit@socialtables.com>
2014-05-24 18:49:21 -0400

    resolving issue

--- public/js/band.js
+++ public/js/band.js
@@ -1,48 +1,45 @@
-
 $(document).ready(function() {
-<<<<<<< Updated upstream
 
-// Map keyboard presses to instrument and note metadata
-var keyPressToNoteElemId = {
-	"piano": {
-		"a": "c",
-		"s": "c-sharp",
-		"d": "d",
-		"f": "d-sharp",
-		"g": "e",
-		"h": "f",
-		"j": "f-sharp",
-		"k": "g",
-		"l": "g-sharp",
-		"z": "a",
-		"x": "a-sharp",
-		"c": "b"
-	},
-	"guitar": {
-		"a": "string-e",
-		"s": "string-b",
-		"d": "string-g",
-		"f": "string-d",
-		"g": "string-a",
-		"h": "string-e",
-		"j": "c",
-		"k": "d",
-		"l": "dm",
-		";": "e"
-	},
-	"drums": {
-		"f": "bass",
-		"g": "snare",
-		"h": "hi-hat",
-		"j": "cymbal"
-	}
-};
+	// Map keyboard presses to instrument and note metadata
+	var keyPressToNoteElemId = {
+		"piano": {
+			"a": "c",
+			"s": "c-sharp",
+			"d": "d",
+			"f": "d-sharp",
+			"g": "e",
+			"h": "f",
+			"j": "f-sharp",
+			"k": "g",
+			"l": "g-sharp",
+			"z": "a",
+			"x": "a-sharp",
+			"c": "b"
+		},
+		"guitar": {
+			"a": "string-e",
+			"s": "string-b",
+			"d": "string-g",
+			"f": "string-d",
+			"g": "string-a",
+			"h": "string-e",
+			"j": "c",
+			"k": "d",
+			"l": "dm",
+			";": "e"
+		},
+		"drums": {
+			"f": "bass",
+			"g": "snare",
+			"h": "hi-hat",
+			"j": "cymbal"
+		}
+	};
 
 
-=======
+
 	var roomId = window.location.pathname.slice(7).toString();
 	console.log("roomId",roomId);
->>>>>>> Stashed changes
 	// connect the socket.io server
 	var socket = io.connect('http://localhost')
 	//define socket events
@@ -52,8 +49,6 @@ var keyPressToNoteElemId = {
 		$('#my-instrument').on('click', function(e){
 			socket.emit('sound',"hey you");
 		});
-		
-
 	});
 
 
