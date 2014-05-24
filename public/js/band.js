6eec134854eb9f7e28b3344c571993e0e901d7c1
Rohit Kalkur <rohit@socialtables.com>
2014-05-24 18:07:19 -0400

    adding in sound object hash

--- public/js/band.js
+++ public/js/band.js
@@ -1,5 +1,41 @@
+// Map keyboard presses to instrument and note metadata
+var keyPressToNoteElemId = {
+	"piano": {
+		"a": "c",
+		"s": "c-sharp",
+		"d": "d",
+		"f": "d-sharp",
+		"g": "e",
+		"h": "f",
+		"j": "f-sharp",
+		"k": "g",
+		"l": "g-sharp",
+		"z": "a",
+		"x": "a-sharp",
+		"c": "b"
+	}
+	"guitar": {
+		"a": "string-e",
+		"s": "string-b",
+		"d": "string-g",
+		"f": "string-d",
+		"g": "string-a",
+		"h": "string-e",
+		"j": "c",
+		"k": "d",
+		"l": "dm",
+		";": "e"
+	}
+	"drums": {
+		"f": "bass",
+		"g": "snare",
+		"h": "high-hat",
+		"j": "cymbal"
+	}
+};
+
 // connect the socket.io server
-var socket = io.connect('http://localhost')
+var socket = io.connect('http://localhost');
 //define socket events
 
 socket.on("connect",function(){
