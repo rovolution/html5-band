46702e498bc196d205948aa22d0ad87400636b1d
Yalcin Dogan <yalcindo1@gmail.com>
2014-05-24 13:18:45 -0400

    folder aded

---
+++ public/javascripts/index.js
@@ -0,0 +1,23 @@
+
+// connect the socket.io server
+var socket = io.connect('http://localhost')
+//define socket events
+
+socket.on("connect",function(){
+	
+
+});
+
+// attach events
+socket.on("serverMessage",function(data){
+
+});
+socket.on("connected",function(data){
+
+});
+socket.on("disconnected",function(data){
+
+});
+socket.on("id",function(data){
+
+});
