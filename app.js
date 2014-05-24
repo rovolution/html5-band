3112a0493909e76d030afd47498143d86092c2c3
Captain Deploy <ed@socialtables.com>
2014-05-24 18:00:27 -0400

    use cookie sessions

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.cookieParser());
+app.use(express.cookieSession({secret: "socialtables"}));
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
