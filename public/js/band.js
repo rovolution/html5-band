b7a9e10d82811bb0946b835f21af3cab92a6bb5a
Rohit Kalkur <rohit@socialtables.com>
2014-05-24 19:21:13 -0400

    sending key-click information via socket.io

--- public/js/band.js
+++ public/js/band.js
@@ -1,38 +1,40 @@
 $(document).ready(function() {
 
-	// Map keyboard presses to instrument and note metadata
-	var keyPressToNoteElemId = {
+	console.log(user);
+
+	// Map keyboard presses to instrument and sound metadata
+	var keyPressToSoundElemId = {
 		"piano": {
-			"a": "c",
-			"s": "c-sharp",
-			"d": "d",
-			"f": "d-sharp",
-			"g": "e",
-			"h": "f",
-			"j": "f-sharp",
-			"k": "g",
-			"l": "g-sharp",
-			"z": "a",
-			"x": "a-sharp",
-			"c": "b"
+			"65": "c",
+			"63": "c-sharp",
+			"68": "d",
+			"70": "d-sharp",
+			"71": "e",
+			"72": "f",
+			"74": "f-sharp",
+			"75": "g",
+			"76": "g-sharp",
+			"90": "a",
+			"88": "a-sharp",
+			"67": "b"
 		},
 		"guitar": {
-			"a": "string-e",
-			"s": "string-b",
-			"d": "string-g",
-			"f": "string-d",
-			"g": "string-a",
-			"h": "string-e",
-			"j": "c",
-			"k": "d",
-			"l": "dm",
-			";": "e"
+			"65": "string-e",
+			"83": "string-b",
+			"68": "string-g",
+			"70": "string-d",
+			"71": "string-a",
+			"72": "string-e",
+			"74": "c",
+			"75": "d",
+			"76": "dm",
+			"186": "e"
 		},
 		"drums": {
-			"f": "bass",
-			"g": "snare",
-			"h": "hi-hat",
-			"j": "cymbal"
+			"70": "bass",
+			"71": "snare",
+			"72": "hi-hat",
+			"74": "cymbal"
 		}
 	};
 
@@ -43,18 +45,28 @@ $(document).ready(function() {
 	// connect the socket.io server
 	var socket = io.connect('http://localhost')
 	//define socket events
+	var myInstrument = document.getElementById("my-instrument").dataset.instrument;
+	var keyCodeToSound = keyPressToSoundElemId[myInstrument];
 
-	socket.on("connect",function(){
-		socket.emit("room",roomId)
-		$('#my-instrument').on('click', function(e){
-			socket.emit('sound',"hey you");
+	socket.on("connect",function() {
+		socket.emit("room",roomId);
+
+		document.addEventListener("keydown", function(e) {
+
+			var data = {
+				user_id: null,
+				instrument: myInstrument,
+				sound: keyCodeToSound[e.keyCode]
+			};
+			socket.emit('sound', data);
 		});
 	});
 
 
 	// attach events
 	socket.on("serverSound",function(data){
-		console.log("sound---------",data);
+		console.log("serverSound!!");
+		console.log(data);
 	});
 	socket.on("connected",function(data){
 
